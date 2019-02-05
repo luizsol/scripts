1dbc0179338038dc77b40989daac0f82305bc1db
Luiz Sol <luizedusol@gmail.com>
2019-02-05 09:02:57 -0200

    Adds new packages and anaconda config

--- linux/arch/packages/anaconda/config.sh
+++ linux/arch/packages/anaconda/config.sh
@@ -1,1 +1,3 @@
-sudo cp /usr/share/terminfo/r/rxvt-unicode-256color /opt/anaconda/share/terminfo/r/
+sudo cp /usr/share/terminfo/r/rxvt-unicode-256color /opt/anaconda/share/terminfo/r/ && \
+sudo chown -R $USER /opt/anaconda/ && \
+echo 'export PATH=/opt/anaconda/bin/:$PATH' >> ~/.bashrc
